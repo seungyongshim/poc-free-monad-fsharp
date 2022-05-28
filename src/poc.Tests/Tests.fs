7e2b7bcc209469ef1b7f3664449a66d126b56bf3
SeungYongShim <syshim@me.com>
2022-05-28 16:01:56 +0900

    .

--- src/poc.Tests/Tests.fs
+++ src/poc.Tests/Tests.fs
@@ -2,7 +2,11 @@ module Tests
 
 open System
 open Xunit
+open poc
 
 [<Fact>]
 let ``My test`` () =
+    let member1 in1 = Free(Member1(in1, Pure))
+    let member2 in2 = Free(Member2(in2, Pure))
+
     Assert.True(true)
--- src/poc/Library.fs
+++ src/poc/Library.fs
@@ -13,8 +13,24 @@ type FaceInstruction<'a> =
     | Member1 of (In1 * (Out1 -> 'a))
     | Member2 of (In2 * (Out2 -> 'a))
 
+type FaceProgram<'a> =
+    | Free of FaceInstruction<FaceProgram<'a>>
+    | Pure of 'a
+
 module FaceInstruction =
-    let map f v =
+    let mapI f v =
         match v with
         | Member1 (x, next) -> Member1 (x, next >> f)
         | Member2 (x, next) -> Member2 (x, next >> f)
+
+module FaceProgram =
+    let rec bind f v =
+        match v with
+        | Free x -> x |> FaceInstruction.mapI (bind f) |> Free
+        | Pure x -> f x
+
+type FaceBuilder () =
+    member this.Bind (x, f) = FaceProgram.bind f x
+    member this.Return x = Pure x
+    member this.ReturnFrom x = x
+    member this.Zero () = Pure ()
