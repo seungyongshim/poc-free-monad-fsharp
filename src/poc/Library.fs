f4db2c3e9ada5fc35766f025efb3ff9877f266b1
SeungYongShim <syshim@me.com>
2022-05-28 15:20:34 +0900

    .

--- src/poc/Library.fs
+++ src/poc/Library.fs
@@ -9,6 +9,12 @@ type IFace =
     abstract member Member1 : input:In1 -> Out1
     abstract member Member2 : input:In2 -> Out2
 
-type FaceInstruction =
-    | Member1 of (In1 * Out1)
-    | Member2 of (In2 * Out2)
+type FaceInstruction<'a> =
+    | Member1 of (In1 * (Out1 -> 'a))
+    | Member2 of (In2 * (Out2 -> 'a))
+
+module FaceInstruction =
+    let map f v =
+        match v with
+        | Member1 (x, next) -> Member1 (x, next >> f)
+        | Member2 (x, next) -> Member2 (x, next >> f)
