fb40d33a101c99e6be170ab5241ae1f770628162
SeungYongShim <syshim@me.com>
2022-05-28 15:16:00 +0900

    .

--- src/poc/Library.fs
+++ src/poc/Library.fs
@@ -1,8 +1,14 @@
 namespace poc
 
 type In1 = string
+type In2 = string
 type Out1 = string
+type Out2 = string
 
 type IFace =
     abstract member Member1 : input:In1 -> Out1
-    abstract member Member2 : input:In1 -> Out1
+    abstract member Member2 : input:In2 -> Out2
+
+type FaceInstruction =
+    | Member1 of (In1 * Out1)
+    | Member2 of (In2 * Out2)
