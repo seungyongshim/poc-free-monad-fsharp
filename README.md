2efe2d8c4584b344200cac76fccd7cbc2edcedf0
SeungYongShim <syshim@me.com>
2022-05-28 15:14:35 +0900

    .

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ![CI](../../workflows/CI/badge.svg) ![Cov](../gh-pages/docs/badge_linecoverage.svg)
 
 * https://blog.ploeh.dk/2017/08/07/f-free-monad-recipe/
+   * OOP로 생각하지 말자.
+   * 인터페이스를 구별된 공용 구조체(discriminated union)로 선언한다.
+   
--- src/poc/Library.fs
+++ src/poc/Library.fs
@@ -1,5 +1,8 @@
-﻿namespace poc
+namespace poc
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type In1 = string
+type Out1 = string
+
+type IFace =
+    abstract member Member1 : input:In1 -> Out1
+    abstract member Member2 : input:In1 -> Out1
